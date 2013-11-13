b7bf2022912325a10c8f84f3ddfe34192a0b0ff7
rolodato <rlopezdato@frba.utn.edu.ar>
2013-11-13 02:13:27 -0300

    mavenizado, api sin get coleccion ni conexion a DB

---
+++ tws-ddl.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS tws;
+CREATE DATABASE tws;
+USE tws;
+
+CREATE TABLE user (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    email varchar(100) NOT NULL,
+    school varchar(100),
+    location varchar(100)
+);
+
+CREATE TABLE lfg (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    owner int NOT NULL,
+    title varchar(100) NOT NULL,
+    studySubject varchar(100) NOT NULL,
+    startTime time,
+    endTime time,
+    FOREIGN KEY (owner)
+        REFERENCES user (id)
+);
